bd1fea6fc56ffa8c5e4631410793a1ecf1b77d6b
joachimchisom1 <joachimchisom1@gmail.com>
2020-10-13 15:27:01 +0100

    added my project repo

---
+++ js/init.js
@@ -0,0 +1,20 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+    // INIT SLIDER {
+  $('.slider').slider({
+    fullWidth: true,
+    indicators: false,
+    height: 500
+  });
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+
